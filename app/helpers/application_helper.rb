dbb419ee6447b5568552a337a4623966303c7adc
Samer Mohamed <samirsuraj@live.com>
2018-02-28 18:14:52 +0200

    refractor css and select colors and nav layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def full_title(title)
+    title + ' | Samer\'s Web'
+  end
+
+  def active_class(expected, given)
+    if(expected==given)
+      return 'active'
+    end
+    ''
+  end
 end
